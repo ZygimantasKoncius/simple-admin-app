7f351f0d9210afe65f3fe30747c4e38bd6c52dae
ZygimantasKoncius <ZygimantasKoncius@github.com>
2018-04-23 22:45:49 +0100

    cleaned something what was used for testing

--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -5,7 +5,6 @@ import './dashboard.html';
 if(Meteor.isClient){
     Template.dashboard.events({
         'click .logout': function(event){
-            console.log('hi');
             event.preventDefault();
             Session.set('page', 'login');
             Meteor.logout();
